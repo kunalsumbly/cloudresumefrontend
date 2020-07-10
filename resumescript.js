2cfdd9d8b62c164fae504d1b73f8d284926d97b4
kusu <kunal.sumbly@sofico.com.au>
2020-07-10 16:18:03 +1000

    changed the baseurl of my endpoint

--- resumescript.js
+++ resumescript.js
@@ -1,14 +1,16 @@
 function incrementVisitCount(){
-    var base_url = "http://localhost:9098";
+    var base_url = "https://eijw6uhj5g.execute-api.us-east-1.amazonaws.com/testdeployapi";
     $('#showcount').append('Visiter Count=');
     var divContent = '';
     var xhr = new XMLHttpRequest();
           xhr.onload = function(){
-            if (xhr.status === 200){
+            if (xhr.status === 200 && "NOK" !== xhr.responseText){
                 responseObject = JSON.parse(xhr.responseText);   
                 console.log(responseObject)
                 divContent+=responseObject;
                 $('#showcount').append(divContent);
+            } else {
+                $('#showcount').append("Not Available");
             }
           }
     // ajax request home tab starts
